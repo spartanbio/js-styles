e76fcee227152264a24a948626bfc3edbaabf2f2
alexkcollier <alexkcollier@gmail.com>
2019-06-17 16:59:24 -0400

    fix: extends config in correct order

--- packages/stylelint-config-scss/stylelint.config.js
+++ packages/stylelint-config-scss/stylelint.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   plugins: ['stylelint-scss'],
-  extends: ['stylelint-config-standard-scss', '@spartanbio/stylelint-config'],
+  extends: [
+    '@spartanbio/stylelint-config',
+    'stylelint-config-standard-scss',
+  ],
   rules: {
+    // For the sake of playing nice with the prettier formatter
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': null,
+
     // scss
     'scss/at-else-empty-line-before': 'never',
     'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
