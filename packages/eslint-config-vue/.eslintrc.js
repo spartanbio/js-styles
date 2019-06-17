eb05e93e7a3585b2bef4b38db4d92be32ec9b70f
alexkcollier <alexkcollier@gmail.com>
2019-06-17 14:15:17 -0400

    fix: enforces component name in template casing

--- packages/eslint-config-vue/.eslintrc.js
+++ packages/eslint-config-vue/.eslintrc.js
@@ -7,6 +7,12 @@ module.exports = {
     'vue/brace-style': ['error', '1tbs', { allowSingleLine: true }],
     'vue/camelcase': ['warn', { ignoreDestructuring: true }],
     'vue/comma-dangle': ['error', 'always-multiline'],
+    'vue/component-name-in-template-casing': [
+      'PascalCase',
+      {
+        registeredComponentsOnly: true
+      }
+    ],
     'vue/eqeqeq': ['error', 'smart'],
     'vue/key-spacing': ['error', { beforeColon: false, afterColon: true }],
     'vue/match-component-file-name': [
