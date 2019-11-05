c9f931115ea326869033bef8d1bdd37618bdb977
alexkcollier <alexkcollier@gmail.com>
2019-11-05 10:10:49 -0500

    fix(eslint-config): removes `eslint-plugin-jest`

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -2,39 +2,70 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    'jest/globals': true,
   },
-  extends: ['standard', 'plugin:jest/recommended'],
-  plugins: ['standard', 'jest'],
+  extends: [
+    'standard',
+  ],
+  plugins: [
+    'standard',
+  ],
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.mjs'],
+        extensions: [
+          '.js',
+          '.mjs',
+        ],
       },
     },
   },
   rules: {
-    'array-bracket-newline': ['error', 'consistent'],
-    'array-element-newline': ['error', 'consistent'],
-    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
-    camelcase: ['warn', { ignoreDestructuring: true }],
-    'comma-dangle': ['error', 'always-multiline'],
-    'max-len': ['error',
+    'array-bracket-newline': [
+      'error',
+      'consistent',
+    ],
+    'array-element-newline': [
+      'error',
+      'consistent',
+    ],
+    'arrow-parens': [
+      2,
+      'as-needed',
+      {
+        requireForBlockBody: true,
+      },
+    ],
+    camelcase: [
+      'warn',
+      {
+        ignoreDestructuring: true,
+      },
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'max-len': [
+      'error',
       {
         code: 100,
         ignoreStrings: true,
         ignoreUrls: true,
-        ignoreComments: true
-      }
+        ignoreComments: true,
+      },
     ],
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 0,
-    'no-return-assign': ['error', 'always'],
-    'object-curly-newline': ['error',
+    'no-return-assign': [
+      'error',
+      'always',
+    ],
+    'object-curly-newline': [
+      'error',
       {
         consistent: true,
         multiline: true,
-      }
+      },
     ],
   },
 }
