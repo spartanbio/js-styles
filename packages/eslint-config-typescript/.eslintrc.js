a8952de78c49e03482ead2d28e57bf21a57c8b3f
Alex Collier <alex.collier@spartanbio.com>
2020-08-21 10:27:32 -0400

    fix(eslint-config-typescript): ensures constructor overloading does not crash eslint

--- packages/eslint-config-typescript/.eslintrc.js
+++ packages/eslint-config-typescript/.eslintrc.js
@@ -7,4 +7,9 @@ module.exports = {
     '@spartanbio/eslint-config',
     'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    // ensures constructor overloading works properly. see https://github.com/typescript-eslint/typescript-eslint/issues/420
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+  },
 };
